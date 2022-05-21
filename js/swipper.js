a2e5bdcfc5e29780e02234cc49ea651067dbaf8d
clinton samuel <88462489+Dr-klint@users.noreply.github.com>
2022-05-21 13:34:43 +0100

    Add files via upload

---
+++ js/swipper.js
@@ -0,0 +1,29 @@
+// import Swiper from "swiper/swiper-bundle.esm.js";
+// import "swiper/swiper-bundle.css";
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    700: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  },
+});
